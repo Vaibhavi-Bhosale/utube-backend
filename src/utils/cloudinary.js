431ffd55cac3747d57a7eac32d0fd4b3915a08f0
vaibhavi-bhosale <vaibhavibhosale.official1@gmail.com>
2025-07-19 06:15:50 -0700

    cloudinary sateup and user middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import {v2 as cloudinary} from cloudinary
+import fs from fs
+
+
+ cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret:  process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary =async (localFilePath)=>{
+      try{
+          if(!localFilePath)  return null;
+
+          //upload file on the cloudinary
+         
+        const uploadResult = await cloudinary.uploader
+       .upload(
+            localFilePath, {
+               resource_type : "auto",
+           }
+       )
+       
+        return uploadResult;
+          
+        console.log("File uploaded successfully !")
+        console.log(uploadResult);
+        console.log(uploadResult.url);
+
+      }
+      catch(err)
+      {
+
+          console.log(err)
+          fs.unlinkSync(localFilePath) //remove the locally save temporary file as upload operation failed
+          return null
+      }
+    
+}
+
+export {uploadOnCloudinary}
