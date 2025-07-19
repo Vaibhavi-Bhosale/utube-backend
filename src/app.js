f47362a4a9173dc40de25ab9c675cebee13845d8
vaibhavi-bhosale <vaibhavibhosale.official1@gmail.com>
2025-07-18 21:54:35 -0700

    add user and video model

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
@@ -10,4 +10,8 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"));
+app.use(cookieParser())
+
 export default app;
